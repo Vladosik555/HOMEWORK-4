169c2ce29480b7401261d037d164bb11ed57ed03
Vladosik555 <144278351+Vladosik555@users.noreply.github.com>
2023-10-05 21:51:13 +0300

    Добавьте файлы проекта.

---
+++ Проект №1/Program.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Проект__1
+{
+    internal class Program
+    {
+        static int Comparison(int a, int b, int result = 0)
+        {
+            if (a > b)
+            {
+                result = a;
+                return result;
+            }
+            else if (a < b)
+            {
+                result = b;
+                return result;
+            }
+            else
+            {
+                result = a = b;
+                return result;
+            }
+        }
+        static void Moving(ref double num1, ref double num2)
+        {
+            double num3 = num1;
+            num1 = num2;
+            num2 = num3;
+        }
+        static int Factorial(int factor)
+        {
+            int result = 1;
+            if (factor == 0)
+            {
+                return result;
+            }
+            for (int i = 1; i <= factor; i++)
+            {
+                result = checked((int)(result * i));
+            }
+            return result;
+        }
+        static int RecursiveFactorial(int fac)
+        {
+            if (fac == 0 || fac == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return checked(fac * RecursiveFactorial(fac - 1));
+            }
+        }
+        static int TwoNOD(int a, int b) 
+        {
+            while (a != b) 
+            {
+                if (a > b) 
+                {
+                    a = a - b;
+                }
+                else
+                {
+                    b = b - a;
+                }
+            }
+             int NOD = b;
+            return NOD;
+        }
+        static int TrheeNOD(int a, int b, int c)
+        {
+            if (a != 0 && b != 0 && c != 0) 
+            {
+                return TwoNOD(TwoNOD(a, b), c);
+            }
+            else
+            {
+                return a + b + c;
+            }
+        }
+        static int Fibanachi(int fib) 
+        {
+            if (fib < 3)
+            {
+                return 1;
+            }
+            else
+            {
+                return Fibanachi(fib - 2) + Fibanachi(fib - 1);
+            }
+        }
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Упражнение 5.1");
+            Console.WriteLine("Данная программа возращает наибольшее число");
+            Console.Write("Напишите первое число: ");
+            bool numb1 = int.TryParse(Console.ReadLine(), out int number1);
+            Console.Write("Напишите второе число: ");
+            bool numb2 = int.TryParse(Console.ReadLine(), out int number2);
+            int otvet = Comparison(number1, number2);
+            Console.Write("Наибольшее число: ");
+            Console.WriteLine(otvet);
+            Console.WriteLine("Для того чтобы продолжить, нажмите на любую клавишу");
+            Console.ReadKey();
+            
+            Console.WriteLine("Упражнение 5.2");
+            Console.WriteLine("Данная программа меняет значения двух передаваемых параметров");
+            Console.Write("Напишите первое число: ");
+            bool a = double.TryParse(Console.ReadLine(), out double num1);
+            Console.Write("Напишите второе число: ");
+            bool b = double.TryParse(Console.ReadLine(), out double num2);
+            Moving(ref num1, ref num2);
+            Console.WriteLine($"Первое значение: {num1}");
+            Console.WriteLine($"Второе значение: {num2}");
+            Console.WriteLine("Для того чтобы продолжить, нажмите на любую клавишу");
+            Console.ReadKey();
+            
+            Console.WriteLine("Упражнение 5.3");
+            Console.WriteLine("Данная программа считает факториал");
+            Console.WriteLine("Напишите число факториала");
+            bool tab = int.TryParse(Console.ReadLine(), out int factoriaal);
+            try
+            {
+                Console.WriteLine($"Ответ: {Factorial(factoriaal)}");
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("Ошибка переполнения");
+            }
+            Console.WriteLine("Для того чтобы продолжить, нажмите на любую клавишу");
+            Console.ReadKey();
+           
+            Console.WriteLine("Упражнение 5.4");
+            Console.WriteLine("Рекурсивный метод факториала");
+            Console.Write("Введите число: ");
+            bool nnumb = int.TryParse(Console.ReadLine(), out int nnumber);
+            try
+            {
+                Console.WriteLine($"Ответ: {RecursiveFactorial(nnumber)}");
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("Ошибка переполнения");
+            }
+            Console.WriteLine("Для того чтобы продолжить, нажмите на любую клавишу");
+            Console.ReadKey();
+
+            
+            Console.WriteLine("Домашнее задание 5.1");
+            Console.WriteLine("Данная программа вычисляет наибольший общий делитель для друх чисел");
+            Console.Write("Введите первое число: ");
+            bool ev = int.TryParse(Console.ReadLine(),out int one);
+            Console.Write("Введите второе число: ");
+            bool ew = int.TryParse(Console.ReadLine(), out int two);
+            Console.WriteLine($"НОД чисел {one} и {two} это: {TwoNOD(one, two)}");
+            Console.WriteLine("Для того чтобы закрыть программу, нажмите на любую клавишу");
+            Console.ReadKey();
+            
+            Console.WriteLine("Данная программа вычисляет наибольший общий делитель для трёх чисел");
+            Console.Write("Введите первое число: ");
+            bool op_1 = int.TryParse(Console.ReadLine(), out int num_1);
+            Console.Write("Введите второе число: ");
+            bool op_2 = int.TryParse(Console.ReadLine(), out int num_2);
+            Console.Write("Введите третье число: ");
+            bool op_3 = int.TryParse(Console.ReadLine(), out int num_3);
+            Console.WriteLine($"Наибольший делитель для чисел {num_1} и {num_2} и {num_3} это: {TrheeNOD(num_1, num_2, num_3)} ");
+            Console.WriteLine("Для того чтобы закрыть программу, нажмите на любую клавишу");
+            Console.ReadKey();
+            
+            Console.WriteLine("Домашнее задание 5.2");
+            Console.WriteLine("Данная программа вычисляет ряд Фибаначи");
+            Console.Write("Напишите порядковый номер ряда Фибаначи: ");
+            bool qwerty = int.TryParse(Console.ReadLine(),out int  num);
+            for (int i = 0; i < num; i++)
+            {
+                Fibanachi(i);
+            }
+            Console.WriteLine($"Ответ: {Fibanachi(num)}");
+            Console.WriteLine("Для того чтобы закрыть программу, нажмите на любую клавишу");
+            Console.ReadKey();
+            
+            
+
+
+
+
+
+        }
+
+    }
+}
---
+++ проект №2/Program.cs
@@ -0,0 +1,240 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace проект__2
+{
+    internal class Program
+    {
+        static int Massiv(ref int mult, out double average, params int[] array1) 
+        {
+            mult = 1;
+            int result = 0;
+            for (int i = 0; i < array1.Length; i++) 
+            {
+                mult *= array1[i];
+                result += array1[i];
+            }
+            average = (double)result / array1.Length;
+            return result;
+        }
+        static void Value(int number)
+        {
+            switch (number)
+            {
+                case 0:
+                    Console.WriteLine("####\n#  #\n#  #\n#  #\n####");
+                    break; 
+                case 1:
+                    Console.WriteLine("#\n#\n#\n#");
+                    break;
+                case 2:
+                    Console.WriteLine(" ## \n#  #\n  # \n # \n####");
+                    break;
+                case 3:
+                    Console.WriteLine("####\n   #\n ###\n   #\n####");
+                    break;
+                case 4:
+                    Console.WriteLine("#  #\n#  #\n#  #\n####\n   #\n   #\n   #");
+                    break;
+                case 5:
+                    Console.WriteLine("####\n# \n####\n   #\n####");
+                    break;
+                case 6:
+                    Console.WriteLine("####\n# \n####\n#  #\n####");
+                    break;
+                case 7:
+                    Console.WriteLine("####\n   #\n  ###\n   #\n   #");
+                    break;
+                case 8:
+                    Console.WriteLine(" ## \n#   #\n#   #\n ## \n#   #\n#   #\n ## ");
+                    break;
+                case 9:
+                    Console.WriteLine("####\n#  #\n####\n   #\n####");
+                    break;
+                default:
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Вы не вошли в промежуток");
+                    Thread.Sleep(3000);
+                    Console.BackgroundColor = ConsoleColor.Black;
+                    break;
+            }
+        }
+        enum Level
+        {
+            Душнила,
+            Ворчаливый,
+            Спокойный
+        }
+        struct Grandfather
+        {
+            public string name;
+            public Level level;
+            public string[] phrases;
+            public int bruises;
+            public int Grandpa(params string[] bruises)
+            {
+                int count = 0;
+                for (int i = 0; i < bruises.Length; i++)
+                {
+                    count++;
+                }
+                return count;
+            }
+        }
+        
+
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Задание номер 1");
+            Console.WriteLine("Данная програма меняет 2 числа местами");
+            Console.WriteLine();
+            Random random = new Random();
+            int[] array = new int[20];
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = random.Next(10001);
+                Console.WriteLine($"{i} элемент массива: {array[i]}");
+            }
+            Console.Write("Введите первое число, которое хотите поменять местами от 0 до 19: ");
+            bool a = int.TryParse(Console.ReadLine(), out int num1);
+            Console.Write("Введите второе число, которое хотите поменять местами от 0 до 19: ");
+            bool b = int.TryParse(Console.ReadLine(), out int num2);
+            if (a && b)
+            {
+                if (num1 == num2)
+                {
+                    Console.WriteLine("Вы ввели одинаковые числа");
+                }
+                else if (num1 > 19 || num2 > 19)
+                {
+                    Console.WriteLine("Вы не вошли в промежуток");
+                }
+                else
+                {
+                    int num3;
+                    num3 = array[num1];
+                    array[num1] = array[num2];
+                    array[num2] = num3;
+                    for (int i = 0; i < array.Length; i++)
+                    {
+                        Console.WriteLine($"{i} элемент массива: {array[i]}");
+                    }
+                }
+            }
+            else 
+            {
+                Console.WriteLine("Нельзя вводить символы");
+            }
+            Console.WriteLine("Для того чтобы продолжить, нажмите на любую клавишу");
+            Console.ReadKey();
+            
+            Console.WriteLine("Задание номер 2");
+            Console.WriteLine("Данная программа считает произведение и среднее арифметичесикое в массиве");
+            int mult = 1;
+            double average = 0;
+            Console.Write("Введите длину массива: ");
+            bool qq = int.TryParse(Console.ReadLine(), out int flag);
+            int[] array1 = new int[flag];
+            for (int i = 0; i < array1.Length ; i++) 
+            {
+                Console.Write("Введите число: ");
+                array1[i] = int.Parse(Console.ReadLine());
+            }
+            Massiv(ref mult, out average, array1);
+            Console.WriteLine($"Умножение: {mult}");
+            Console.WriteLine($"Среднее арифметическое значение: {average}");
+            Console.WriteLine("Для того чтобы продолжить, нажмите на любую клавишу");
+            Console.ReadKey();
+            
+            Console.WriteLine("Задание номер 3");
+            Console.WriteLine("Число от 0 до 9");
+            Console.Write("Введите любое значение: ");
+            while (true) 
+            {
+                string word = Console.ReadLine();
+                bool ban = int.TryParse(word, out int num10);
+                if (ban)
+                {
+                    Value(num10);
+                }
+                else if (word == "exit" || word == "закрыть")
+                {
+                    break;
+                }
+                else 
+                {
+                    try 
+                    {
+                        throw new Exception();
+                    }
+                    catch (Exception)
+                    {
+                        Console.WriteLine("Исключение");
+                    }
+
+                }
+            }
+            Console.WriteLine("Для того чтобы продолжить, нажмите на любую клавишу");
+            Console.ReadKey();
+            
+            Console.WriteLine("Задание номер 4");
+            Console.WriteLine("Программа деды с матюками");
+            string[] Ivan_phrases = new string[] { "Козлы вы ебу**е", "бл*ди драные", "уеб***те отсюда"};
+            string[] Alexander_phrases = new string[] {"Вали на*** из моего дома"};
+            string[] Mitrofan_phrases = new string[] {"ты че сидешь на***", "чтобы у тебя сарай сгорел к хер**"};
+            string[] Vyacheslav_phrases = new string[] {};
+            string[] Arcadiy_phrases = new string[] {"Вставай пизд**ыш"};
+            
+            Grandfather Ivan = new Grandfather();
+            Ivan.name = "Иван";
+            Ivan.level = Level.Душнила;
+            Ivan.phrases = Ivan_phrases;
+            Ivan.bruises = 3;
+            Ivan.Grandpa(Ivan_phrases);
+
+            Grandfather Alexander = new Grandfather();
+            Alexander.name = "Александр";
+            Alexander.level = Level.Ворчаливый;
+            Alexander.phrases = Alexander_phrases;
+            Alexander.bruises = 1;
+            Alexander.Grandpa(Alexander_phrases);
+
+            Grandfather Mitrofan = new Grandfather();
+            Mitrofan.name = "Митрофан";
+            Mitrofan.level = Level.Ворчаливый;
+            Mitrofan.phrases = Mitrofan_phrases;
+            Mitrofan.bruises = 2;
+            Mitrofan.Grandpa(Mitrofan_phrases);
+
+            Grandfather Vyacheslav = new Grandfather();
+            Vyacheslav.name = "Вячеслав";
+            Vyacheslav.level = Level.Спокойный;
+            Vyacheslav.phrases = Vyacheslav_phrases;
+            Vyacheslav.bruises = 0;
+            Vyacheslav.Grandpa(Vyacheslav_phrases);
+
+            Grandfather Arcadiy = new Grandfather();
+            Arcadiy.name = "Аркадий";
+            Arcadiy.level = Level.Ворчаливый;
+            Arcadiy.phrases = Arcadiy_phrases;
+            Arcadiy.bruises = 1;
+            Arcadiy.Grandpa(Arcadiy_phrases);
+            Console.WriteLine($"{Ivan.name} получил {Ivan.Grandpa(Ivan_phrases)} фингалов от бабки");
+            Console.WriteLine($"{Alexander.name} получил {Alexander.Grandpa(Alexander_phrases)} фингалов от бабки");
+            Console.WriteLine($"{Mitrofan.name} получил {Mitrofan.Grandpa(Mitrofan_phrases)} фингалов от бабки");
+            Console.WriteLine($"{Mitrofan.name} получил {Vyacheslav.Grandpa(Vyacheslav_phrases)} фингалов от бабки");
+            Console.WriteLine($"{Vyacheslav.name} получил {Arcadiy.Grandpa(Arcadiy_phrases)} фингалов от бабки");
+            Console.ReadKey();
+
+
+
+
+        }
+    }
+}
